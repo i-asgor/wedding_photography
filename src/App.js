4323793ad932094ff3e36b3a68f37c9320520378
Md Asgor Office <asgor.ice@gmail.com>
2022-04-19 15:46:01 +0600

    Header Title & Navbar setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Pages/Home/Home/Home';
+import About from './Pages/About/About';
+import Blogs from './Pages/Blogs/Blogs';
+import Login from './Pages/Login/Login/Login';
+import Registration from './Pages/Login/Registration/Registration';
+import NotFound from './Pages/Shared/NotFound/NotFound';
+import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
-        
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/registration' element={<Registration />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
