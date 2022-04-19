e0ec97b41062d49cb2354a9a83c22121edb4b660
Md Asgor Office <asgor.ice@gmail.com>
2022-04-19 15:59:02 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className='bg-dark'>
+            <p className='text-white'>@ {year} copyright wedding photography</p>
         </div>
     );
 };
